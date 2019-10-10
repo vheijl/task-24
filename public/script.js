d1ed4f864c5084561462e27a754f8f2b19ea8e4d
Victor Heijler <victor.heijler@se.experis.com>
2019-10-10 16:04:40 +0200

    Add some basic information from the ajax and adjust css and the json data model

--- public/script.js
+++ public/script.js
@@ -1,5 +1,4 @@
 window.addEventListener("load", init);
-let $ = document.getElementById;
 
 async function init() {
     let json = await fetchData();
@@ -15,45 +14,70 @@ async function fetchData() {
 function createElements(obj) {
     createPresentation(obj["presentation"]);
     createContactInfo(obj["contact-information"]);
-    createEducation(obj["education"]);
-    createWorkExp(obj["work-experience"]);
-    createSkills(obj["skills"]);
-    createExtra(obj["extra"]);
+    // createEducation(obj["education"]);
+    // createWorkExp(obj["work-experience"]);
+    // createSkills(obj["skills"]);
+    // createExtra(obj["extra"]);
 }
 
 function createPresentation(presentation) {
-    let imgEl = $("");
-    let nameEl = $("");
-    let ageEl = $("");
-    let titleEl = $("");
-    let cityEl = $("");
-    let countryEl = $("");
-    let languageEl = $("");
-    let summaryEl = $("");
+    let imgEl = document.getElementById("pic");
+    let nameEl = document.getElementById("name");
+    let ageEl = document.getElementById("age");
+    let titleEl = document.getElementById("title");
+    let cityEl = document.getElementById("city");
+    let languageEl = document.getElementById("lang");
+    let summaryEl = document.getElementById("sum");
 
     imgEl.src = presentation["image"];
-    nameEl.textContent = presentation["name"];
+    nameEl.innerHTML += presentation["name"];
+    ageEl.innerHTML  += presentation["age"] + " years old";
+    titleEl.innerHTML += presentation["title"];
+    cityEl.innerHTML += presentation["city"] + ", " + presentation["country"];
+    languageEl.appendChild(createLanguageList(presentation["language"]));
+    summaryEl.innerHTML += presentation["summary"];
 
 }
 
-function createContactInfo() {
-    let emailEl =  $("");
-    let phoneEl =  $("");
-    let linksEl =  $("");
+function createLanguageList(arr) {
+    let ul = document.createElement("ul");
+    for (let i = 0; i < arr.length; i++) {
+        let li = document.createElement("li");
+        let em = document.createElement("em");
+        li.innerHTML = arr[i]["language"] + " - ";
+        em.innerHTML = arr[i]["language-level"];
+        li.appendChild(em);
+        ul.appendChild(li);
+    }
+    return ul;
 }
 
-function createEducation() {
-    let educationList = $("");
+function createContactInfo(contactInfo) {
+    let emailEl =  document.getElementById("mail");
+    let phoneEl =  document.getElementById("phone");
+    let linksEl =  document.getElementById("links");
+
+    emailEl.innerHTML = contactInfo["email"];
+    phoneEl.innerHTML = contactInfo["phone"];
+    // linksEl.appendChild(createLinksList(contactInfo["links"]));
 }
 
-function createWorkExp() {
-    let workplaceList = $("");
+function createLinksList(arr) {
+
 }
 
-function createSkills() {
-    let skills = $("");
-}
+// function createEducation(education) {
+//     let educationList = document.getElementById("");
+// }
 
-function createExtra() {
-    let extraList = $("");
-}
+// function createWorkExp(workExp) {
+//     let workplaceList = document.getElementById("");
+// }
+
+// function createSkills(skills) {
+//     let skills = document.getElementById("");
+// }
+
+// function createExtra(extra) {
+//     let extraList = document.getElementById("");
+// }
