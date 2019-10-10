69508ac7d9c87ad6c8a2e8e4766151541a89f08f
Victor Heijler <victor.heijler@se.experis.com>
2019-10-10 14:29:04 +0200

    Basic script.js boilerplate

--- public/script.js
+++ public/script.js
@@ -1,1 +1,59 @@
-console.log ("hej");
+window.addEventListener("load", init);
+let $ = document.getElementById;
+
+async function init() {
+    let json = await fetchData();
+    createElements(json);
+}
+
+async function fetchData() {
+    const response = await fetch('/data');
+    const myJson = await response.json();
+    return myJson;
+}
+
+function createElements(obj) {
+    createPresentation(obj["presentation"]);
+    createContactInfo(obj["contact-information"]);
+    createEducation(obj["education"]);
+    createWorkExp(obj["work-experience"]);
+    createSkills(obj["skills"]);
+    createExtra(obj["extra"]);
+}
+
+function createPresentation(presentation) {
+    let imgEl = $("");
+    let nameEl = $("");
+    let ageEl = $("");
+    let titleEl = $("");
+    let cityEl = $("");
+    let countryEl = $("");
+    let languageEl = $("");
+    let summaryEl = $("");
+
+    imgEl.src = presentation["image"];
+    nameEl.textContent = presentation["name"];
+
+}
+
+function createContactInfo() {
+    let emailEl =  $("");
+    let phoneEl =  $("");
+    let linksEl =  $("");
+}
+
+function createEducation() {
+    let educationList = $("");
+}
+
+function createWorkExp() {
+    let workplaceList = $("");
+}
+
+function createSkills() {
+    let skills = $("");
+}
+
+function createExtra() {
+    let extraList = $("");
+}
